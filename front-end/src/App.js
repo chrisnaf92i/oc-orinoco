214a7982d3d61047f7f8fa7056f2a1245db2f945
christopher nafrere <nafrere.christopher@gmail.com>
2022-04-21 17:09:05 +0200

    création du header

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,7 +5,20 @@ import Acceuil from './pages/Acceuil';
 function App() {
   return (
     <div className="App">
+      <header className="App-header">
+        <h1>Orinoco</h1>
 
+        <nav>
+          <Link to="/acceuil"> <h2>Acceuil</h2></Link>
+          <Link to="/pannier"> <h2>Pannier</h2> </Link>
+          <Link to="/commander"> <h2>Commander</h2> </Link>
+        </nav>
+      </header>
+
+      <Routes>
+        <Route exact path="/" element={<Acceuil/>}/>
+        <Route path="/acceuil" element={<Acceuil/>}/>
+      </Routes>
     </div>
   );
 }
