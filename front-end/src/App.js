10f071782fcf11bda3c86c3c2c57b2d9d8d10dba
christopher nafrere <nafrere.christopher@gmail.com>
2022-04-21 13:06:24 +0200

    first commit

---
+++ front-end/src/App.js
@@ -0,0 +1,13 @@
+import { Link, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Acceuil from './pages/Acceuil';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
